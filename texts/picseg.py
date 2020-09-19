cf6eb287af58429cf869451a46a3cdf9f8c7ba7e
tokits <tokit_s@nifty.com>
2020-09-19 20:10:59 +0900

    練習問題2.44（図形言語）を追加

---
+++ texts/picseg.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from calysto.graphics import *
+from calysto.display import display, clear_output
+
+#image_width=512
+image_width=256
+canvas = Canvas(size=(image_width, image_width))
+
+# 線分を書く
+def draw_line(p1_x, p1_y, p2_x, p2_y):
+    def conv(z):
+        return (image_width * z)
+    start=(conv(p1_x), image_width - conv(p1_y))
+    end=(conv(p2_x), image_width - conv(p2_y))
+    line=Line(start, end)
+    #line.extras["stroke"] =Color(255, 0, 0)
+    canvas.draw(line)
+
+# キャンバスのクリア
+def clear():
+    canvas.clear()
+
+# キャンバスへの反映
+def update():
+    display(canvas)
